59049b2c41a8686639be1612be5be2d5b1c8973a
Joshua Yuan <hamburgerbun@gmail.com>
2015-05-07 23:12:05 -0500

    added ethan app and urls for the two pages we care about

--- ethan_django/ethan_django/urls.py
+++ ethan_django/ethan_django/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'ethan_django.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'ethan_django.views.home_page', name='home')
+    url(r'^/game', 'ethan_django.views.game_page', name='game')
 )
