0ad1b5ef1e54b487e03dce1872493bf5d4f0265f
Joshua Yuan <hamburgerbun@gmail.com>
2015-05-18 22:38:08 -0500

    added the model for the game, players, and turns

--- ethan_django/ethan/models.py
+++ ethan_django/ethan/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    game_id = models.CharField(max_length=32)
+    scores = models.CommaSeparatedIntegerField(max_length=30*4)  # array of scores integers
+    turn_number = models.IntegerField()
+    win_lose_ind = models.IntegerField() #0 nothing, -1 ethan wins, else player num win
+    last_updated = models.DateTimeField() #date of last access
+    created = models.DateTimeField() #date created
+
+class Player(models.Model):
+    game_id = models.CharField(max_length=32)
+    player_num = models.IntegerField()
+    player_name = models.CharField(max_length=40)
+
+class Turn(models.Model):
+    game_id = models.CharField(max_length=32)
+    turn_num = models.IntegerField()
+    turn_str = models.CharField(max_length=50)
